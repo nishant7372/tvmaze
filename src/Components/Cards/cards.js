50bad4dde80ebd46cb11291b5aca21f595893b69
NishantSingh02 <nst7372@gmail.com>
2023-04-06 19:19:37 +0530

    hook used

--- src/Components/Cards/cards.js
+++ src/Components/Cards/cards.js
@@ -1,68 +1,26 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ShowCard from "./showCard";
 import "./cards.css";
 import Spinner from "./Spinner";
-// import axios from "axios";
+
+import { useGetShows } from "../../hooks/useGetShows";
 
 export default function Cards() {
-  // const alertUser = (e) => {
-  //   e.preventDefault();
-  //   e.returnValue = "";
-  // };
+  const { getShows, isPending } = useGetShows();
 
-  // useEffect(() => {
-  //   window.addEventListener("beforeunload", alertUser);
-  //   return () => {
-  //     window.removeEventListener("beforeunload", alertUser);
-  //   };
-  // }, []);
+  const { query } = useParams();
 
-  const queryString = useLocation().search;
-  const queryParams = new URLSearchParams(queryString);
-  const query = queryParams.get("q");
-
-  const [isPending, setIsPending] = useState(false);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
 
-  // async function fetchData() {
-  //   setIsPending(true);
-  //   try {
-  //     const response = await axios.get(
-  //       `https://api.tvmaze.com/search/shows?q=${query}`
-  //     );
-  //     setData(response.data);
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     setError(error);
-  //   }
-  //   setIsPending(false);
-  // }
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, [query]);
-
   useEffect(() => {
-    setIsPending(true);
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
-      .then((res) => res.json())
-      .then((results) => {
-        if (results.length === 0) {
-          setError("No result found...");
-          setIsPending(false);
-        } else {
-          setError(null);
-        }
-        setData(results);
-        setIsPending(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsPending(false);
-      });
+    const fetch = async () => {
+      const res = await getShows(query);
+      if (res.ok) setData(res.data);
+      else if (res.error) setError(res.error);
+    };
+    fetch();
   }, [query]);
 
   return (
